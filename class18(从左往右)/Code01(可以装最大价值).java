4f21b2cff3d31763cfeaae1a278c912b16dbaf1c
Malcolm <MaloclmSelf@Outlook.com>
2023-03-05 00:58:25 +0800

    从左网右

--- class18(从左往右)/Code01(可以装最大价值).java
+++ class18(从左往右)/Code01(可以装最大价值).java
@@ -16,6 +16,7 @@ public class Code01_Knapsack {
 
 	// index 0~N
 	// rest 负~bag
+	// w=重量 v=价值
 	public static int process(int[] w, int[] v, int index, int rest) {
 		if (rest < 0) {
 			return -1;
@@ -23,6 +24,7 @@ public class Code01_Knapsack {
 		if (index == w.length) {
 			return 0;
 		}
+		//算货物价值
 		int p1 = process(w, v, index + 1, rest);
 		int p2 = 0;
 		int next = process(w, v, index + 1, rest - w[index]);
